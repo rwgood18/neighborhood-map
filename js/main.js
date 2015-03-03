39ecc8b588593a12028b06f8461f9b8d92f35134
Rusty Good <rwgood18@yahoo.com>
2015-03-03 15:57:05 -0600

    Add map to back ground

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
-//js
+var map;
+var infowindow;
+
+function initialize() {
+  var kc = new google.maps.LatLng(39.097279,-94.585722);
+
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
+    center: kc,
+    zoom: 15
+  });
+
+  var request = {
+    location: kc,
+    radius: 500,
+    types: ['establishment']
+  };
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
