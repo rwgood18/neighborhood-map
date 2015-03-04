ee1c11ba8755573a9a5b3fbc6913cd0aa86862d8
Rusty Good <rwgood18@yahoo.com>
2015-03-03 20:35:11 -0600

    Add font-effect-shadow-multiple

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function initialize() {
   var kc = new google.maps.LatLng(39.097279,-94.585722);
   var mapOptions = {
     center: kc,
-    zoom: 15
+    zoom: 16
   };
   var map = new google.maps.Map(
       document.getElementById('map-canvas'), mapOptions);
@@ -18,57 +18,3 @@ function initialize() {
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
-
-
-
-
-
-
-/*
-var map;
-var infowindow;
-
-function initialize() {
-  var kc = new google.maps.LatLng(39.097279,-94.585722);
-
-  var request = {
-    location: kc,
-    radius: 500,
-    types: ['establishment']
-  };
-
-  map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: kc,
-    zoom: 15,
-  });
-
-  infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
-}
-
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
-==========================================================================
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]);
-    }
-  }
-}
-
-function createMarker(place) {
-  var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
-}
-*/
