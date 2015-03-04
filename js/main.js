375efd9923e2824dfe0d302c0f8220a0743b98e6
Rusty Good <rwgood18@yahoo.com>
2015-03-03 20:03:05 -0600

    Add panorama to map

--- js/main.js
+++ js/main.js
@@ -1,24 +1,56 @@
+function initialize() {
+  var kc = new google.maps.LatLng(39.097279,-94.585722);
+  var mapOptions = {
+    center: kc,
+    zoom: 15
+  };
+  var map = new google.maps.Map(
+      document.getElementById('map-canvas'), mapOptions);
+  var panoramaOptions = {
+    position: kc,
+    pov: {
+      heading: 34,
+      pitch: 10
+    }
+  };
+  var panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), panoramaOptions);
+  map.setStreetView(panorama);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
+
+
+/*
 var map;
 var infowindow;
 
 function initialize() {
   var kc = new google.maps.LatLng(39.097279,-94.585722);
 
-  map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: kc,
-    zoom: 15
-  });
-
   var request = {
     location: kc,
     radius: 500,
     types: ['establishment']
   };
+
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
+    center: kc,
+    zoom: 15,
+  });
+
   infowindow = new google.maps.InfoWindow();
   var service = new google.maps.places.PlacesService(map);
   service.nearbySearch(request, callback);
 }
 
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+==========================================================================
 function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
@@ -39,5 +71,4 @@ function createMarker(place) {
     infowindow.open(map, this);
   });
 }
-
-google.maps.event.addDomListener(window, 'load', initialize);
+*/
