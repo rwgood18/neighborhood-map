d5e48c9359bafdf50179959b76abe8a2a7422c5a
Rusty Good <rwgood18@yahoo.com>
2015-03-10 12:27:09 -0500

    Add Place() class and iterate over data.places array to create custom map markers

--- js/main.js
+++ js/main.js
@@ -1,15 +1,88 @@
 var data = {
   places: [
-    {name: 'Kauffman Center for the Performing Arts',
-    LatLng: '39.094114,-94.587513',
-    apiInfo: ''},
-    {name: 'Sprint Center'}
+    {
+    name: 'Kauffman Center for the Performing Arts',
+    Lat: 39.094114,
+    Lng: -94.587513,
+    apiInfo: ''
+    },
+    {
+    name: 'Sprint Center',
+    Lat: 39.098403,
+    Lng: -94.580485,
+    apiInfo: ''
+    },
+    {
+    name: 'KC Live!',
+    Lat: 39.098036,
+    Lng: -94.581644,
+    apiInfo: ''
+    },
+    {
+    name: 'Bartle Hall',
+    Lat: 39.098611,
+    Lng: -94.587105,
+    apiInfo: ''
+    },
+    {
+    name: 'Folly Theater',
+    Lat: 39.100426,
+    Lng: -94.587212,
+    apiInfo: ''
+    },
+    {
+    name: 'Municipal Auditorium',
+    Lat: 39.098095,
+    Lng: -94.586407,
+    apiInfo: ''
+    },
+    {
+    name: 'The Midland',
+    Lat: 39.099069,
+    Lng: -94.583704,
+    apiInfo: ''
+    },
+    {
+    name: 'Kansas City Marriott',
+    Lat: 39.101159,
+    Lng: -94.58628,
+    apiInfo: ''
+    },
+    {
+    name: 'National World War I Museum at Liberty Memorial',
+    Lat: 39.08134,
+    Lng: -94.585937,
+    apiInfo: ''
+    },
+    {
+    name: 'Union Station',
+    Lat: 39.085396,
+    Lng: -94.585474,
+    apiInfo: ''
+    },
+    {
+    name: 'Crown Center',
+    Lat: 39.083314,
+    Lng: -94.582127,
+    apiInfo: ''
+    },
   ]
-}
+};
 
-
+var Place = function (info) {
+  this.name = ko.observable(info.name);
+  this.Lat = ko.observable(info.Lat);
+  this.Lng = ko.observable(info.Lng);
+};
 
 function ViewModel () {
+  var self = this;
+
+  this.places = ko.observableArray([]);
+
+  data.places.forEach(function(info) {
+    self.places.push( new Place(info));
+  })
 
   function initialize() {
     var kc = new google.maps.LatLng(39.097279,-94.585722);
@@ -18,15 +91,17 @@ function ViewModel () {
       zoom: 16
     };
     var map = new google.maps.Map(
-        document.getElementById('map-canvas'), mapOptions);
+      document.getElementById('map-canvas'), mapOptions);
+    var pLen = data.places.length;
+    for (i = 0; i < pLen; i++) {
+      var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(data.places[i].Lat, data.places[i].Lng),
+          map: map,
+          title: data.places[i].name
+      });
+    }
 
-    var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(39.094114,-94.587513),
-        map: map,
-        title: 'Hello World!'
-    });
-
-    var panoramaOptions = {ko.applyBindings(new ViewModel());
+    var panoramaOptions = {
       position: kc,
       pov: {
         heading: 34,
