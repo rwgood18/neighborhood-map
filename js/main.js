44cbb2644f1d3c6c0cbad5e4adc54ed140ce54ba
Rusty Good <rwgood18@yahoo.com>
2015-03-16 14:16:21 -0500

    Get Foursquare returning tips and images about each venue when user clicks on the venue marker

--- js/main.js
+++ js/main.js
@@ -4,85 +4,85 @@ var data = {
     name: 'Kauffman Center for the Performing Arts',
     Lat: 39.094114,
     Lng: -94.587513,
-    id: '4bc4e070abf4952177b4c593'
+    vid: '4bc4e070abf4952177b4c593'
     },
     {
     name: 'Sprint Center',
     Lat: 39.098403,
     Lng: -94.580485,
-    id: '4abb8024f964a520df8320e3'
+    vid: '4abb8024f964a520df8320e3'
     },
     {
     name: 'KC Live!',
     Lat: 39.098036,
     Lng: -94.581644,
-    id: '4c34bf94a0ced13a83b0186e'
+    vid: '4c34bf94a0ced13a83b0186e'
     },
     {
     name: 'Bartle Hall',
     Lat: 39.098611,
     Lng: -94.587105,
-    id: '4c03d3c6f56c2d7fb1591d66'
+    vid: '4c03d3c6f56c2d7fb1591d66'
     },
     {
     name: 'Folly Theater',
     Lat: 39.100426,
     Lng: -94.587212,
-    id: '4ad4c01ff964a520eff120e3'
+    vid: '4ad4c01ff964a520eff120e3'
     },
     {
     name: 'Municipal Auditorium',
     Lat: 39.098095,
     Lng: -94.586407,
-    id: '4ad4c01ff964a520f2f120e3'
+    vid: '4ad4c01ff964a520f2f120e3'
     },
     {
     name: 'The Midland',
     Lat: 39.099069,
     Lng: -94.583704,
-    apiInfo: '4ad4c01ff964a520eef120e3'
+    vid: '4ad4c01ff964a520eef120e3'
     },
     {
     name: 'Kansas City Marriott',
     Lat: 39.101159,
     Lng: -94.58628,
-    id: '4ada400af964a520872021e3'
+    vid: '4ada400af964a520872021e3'
     },
     {
     name: 'National World War I Museum at Liberty Memorial',
     Lat: 39.08134,
     Lng: -94.585937,
-    id: '4f9c0ba5e4b04dd7353a754d'
+    vid: '4f9c0ba5e4b04dd7353a754d'
     },
     {
     name: 'Union Station',
     Lat: 39.085396,
     Lng: -94.585474,
-    id: '4f885099e4b0cec3aa2c928a'
+    vid: '4f885099e4b0cec3aa2c928a'
     },
     {
     name: 'Crown Center',
     Lat: 39.083314,
     Lng: -94.582127,
-    id: '4ad4c01ff964a520fff120e3'
+    vid: '4ad4c01ff964a520fff120e3'
     },
     {
     name: 'Kansas City Public Library',
     Lat: 39.116743,
     Lng: -94.583466,
-    id: '4b13eb34f964a5207a9a23e3'
+    vid: '4b13eb34f964a5207a9a23e3'
     },
     {
     name: 'Nelson-Atkins Museum of Art',
     Lat: 39.045161,
     Lng: -94.580914,
-    id: '4ad4c01ef964a520aff120e3'
+    vid: '4ad4c01ef964a520aff120e3'
     },
     {
     name: 'Uptown Theater',
     Lat: 39.061349,
     Lng: -94.590645,
-    id: '4ad4c01ff964a520f1f120e3'
+    vid: '4ad4c01ff964a520f1f120e3'
     }
   ]
 };
@@ -91,6 +91,7 @@ var Place = function (info) {
   this.name = ko.observable(info.name);
   this.Lat = ko.observable(info.Lat);
   this.Lng = ko.observable(info.Lng);
+  this.vid = info.vid;
 };
 
 function ViewModel () {
@@ -127,9 +128,11 @@ function ViewModel () {
           title: data.places[i].name
       });
 
-      google.maps.event.addListener(marker, 'click', function() {
-        getFoursquare();
-      });
+      google.maps.event.addListener(marker, 'click', (function(icopy) {
+        return function () {
+          getFoursquare(data.places[icopy]);
+        }
+      })(i))
     }
 
     var panoramaOptions = {
@@ -169,18 +172,15 @@ function ViewModel () {
     });   
   })
  
-  getFoursquare = function () {
+  getFoursquare = function (place) {
     infoChanger();
-    console.log("getFoursquare called");
-    $.getJSON('https://api.foursquare.com/v2/venues/4fe9d9bea17c0739a860b879/photos?&client_id=IISH2ZQK5FLY3F4GM0P4SUQN4EXQV5ZENQMBSD1POZ0AABOO&client_secret=OKCZBXIZOLZI4E1M55VIOSD2CL3UH1YJAMDPEXWR1FFLXNDZ&v=20150305', function (response) {
+    $.getJSON('https://api.foursquare.com/v2/venues/' + place.vid + '/photos?&client_id=IISH2ZQK5FLY3F4GM0P4SUQN4EXQV5ZENQMBSD1POZ0AABOO&client_secret=OKCZBXIZOLZI4E1M55VIOSD2CL3UH1YJAMDPEXWR1FFLXNDZ&v=20150305', function (response) {
       $('#img0').attr('src', response.response.photos.items[0].prefix + "200x200" + response.response.photos.items[0].suffix);
       $('#img1').attr('src', response.response.photos.items[1].prefix + "200x200" + response.response.photos.items[1].suffix);
       $('#img2').attr('src', response.response.photos.items[2].prefix + "200x200" + response.response.photos.items[2].suffix);
-      //console.log(response.response.photos.items[0].prefix + "200x200" + response.response.photos.items[0].suffix);
     })
 
-    $.getJSON('https://api.foursquare.com/v2/venues/4fe9d9bea17c0739a860b879/tips?&client_id=IISH2ZQK5FLY3F4GM0P4SUQN4EXQV5ZENQMBSD1POZ0AABOO&client_secret=OKCZBXIZOLZI4E1M55VIOSD2CL3UH1YJAMDPEXWR1FFLXNDZ&v=20150305', function (response) {
-      console.log(response);
+    $.getJSON('https://api.foursquare.com/v2/venues/' + place.vid + '/tips?&client_id=IISH2ZQK5FLY3F4GM0P4SUQN4EXQV5ZENQMBSD1POZ0AABOO&client_secret=OKCZBXIZOLZI4E1M55VIOSD2CL3UH1YJAMDPEXWR1FFLXNDZ&v=20150305', function (response) {
       $('#p0').text(response.response.tips.items[0].text);
       $('#p1').text(response.response.tips.items[1].text);
       $('#p2').text(response.response.tips.items[2].text);
