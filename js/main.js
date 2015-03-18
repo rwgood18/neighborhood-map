c86ccc5a4051ad545166478e414bb02178dfb914
Rusty Good <rwgood18@yahoo.com>
2015-03-17 22:01:50 -0500

    Add comments to main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//collection of venues
 var data = {
   places: [
     {
@@ -87,6 +88,7 @@ var data = {
   ]
 };
 
+//instantiate each venue as a Place
 var Place = function (info) {
   this.name = ko.observable(info.name);
   this.Lat = ko.observable(info.Lat);
@@ -94,24 +96,28 @@ var Place = function (info) {
   this.vid = info.vid;
 };
 
+//ViewModel
 function ViewModel () {
   var self = this;
 
-  self.places = ko.observableArray([]);
+  //initialize observable arrays
   self.buffer = ko.observableArray([]);
   self.names = ko.observableArray([]);
 
+  //add content to observable arrays
   data.places.forEach(function(info) {
-    self.places.push( new Place(info));
     self.buffer.push( new Place(info));
   })
-
   data.places.forEach(function(info) {
     self.names.push( info.name);
   })
-
+  //initialize search bar with search instructions
   this.searchString = ko.observable("Search for a Venue");
+
+  //create variable for number of places in data.places
   var pLen = data.places.length;
+
+  //create the background map
   initialize = function () {
     var kc = new google.maps.LatLng(39.092279,-94.589722);
     var mapOptions = {
@@ -121,6 +127,7 @@ function ViewModel () {
     var map = new google.maps.Map(
       document.getElementById('map-canvas'), mapOptions);
     
+    //add marker for each location in data.places
     for (i = 0; i < pLen; i++) {
       var marker = new google.maps.Marker({
           position: new google.maps.LatLng(data.places[i].Lat, data.places[i].Lng),
@@ -128,6 +135,7 @@ function ViewModel () {
           title: data.places[i].name
       });
 
+      //return a function that calls get foursquare() when user clicks a marker
       google.maps.event.addListener(marker, 'click', (function(icopy) {
         return function () {
           getFoursquare(data.places[icopy]);
@@ -135,6 +143,7 @@ function ViewModel () {
       })(i))
     }
 
+    //create streetview element
     var panoramaOptions = {
       position: kc,
       pov: {
@@ -146,27 +155,31 @@ function ViewModel () {
     map.setStreetView(panorama);
   }
 
+  //change to streetview
   viewChanger = function () {
     $('#pano').css('display', 'block');
     $('#info').css('display', 'none');
   }
 
+  //change to info view
   infoChanger = function () {
     $('#info').css('display', 'block');
     $('#pano').css('display', 'none');
   }
 
+  //clear text in search bar when user clicks to make a new search
   clearText = function () {
     this.searchString("");
     $('#search').css('color', 'black');
   }
   
+  //autocomplete functionality for search bar
   $(function() {
     $( "#search" ).autocomplete({
       appendTo: ".place-list",
       source: self.names(),
-      response: function( event, ui ) {
-      },
+
+      //this function is called when the user selects a suggestion from the list
       select: function (event, ui) {
         var x = window.setTimeout(function(){
           search();
@@ -175,6 +188,7 @@ function ViewModel () {
     });
   })
 
+  //search() is called when the user clicks the search button
   search = function () {
     var term = $('#search').val();
     for (var i=0; i<pLen; i++) {
@@ -184,27 +198,35 @@ function ViewModel () {
     }
   }
 
+  //called when user clicks on a venue from the venue list
   preGetFoursquare = function () {
     getFoursquare(this);
   }
- 
+  
+  //makes a 2 requests to the foursquare api
   getFoursquare = function (place) {
     infoChanger();
+
+    //hide instructions
     $('#pre-click').css('display', 'none');
+
+    //request for images
     $.ajax({ url: 'https://api.foursquare.com/v2/venues/' + place.vid + '/photos?&client_id=IISH2ZQK5FLY3F4GM0P4SUQN4EXQV5ZENQMBSD1POZ0AABOO&client_secret=OKCZBXIZOLZI4E1M55VIOSD2CL3UH1YJAMDPEXWR1FFLXNDZ&v=20150305', 
       success: function (response) {
+        //if there are no available images
         if (response.response.photos.items.length == 0) {
           $('.img-error').text('There are no available images for this venue.');
           for (var i=0; i<3; i++) {
             $('#img' + i).attr('src', '');
           }
         } else {
+          //set image sources to new image urls
           $('.img-error').text('');
           for (var i=0; i<3; i++) {
             $('#img' + i).attr('src', response.response.photos.items[i].prefix + "200x200" + response.response.photos.items[i].suffix);
           }
         }
-
+        //If there are images, show the image divs. If not, don't.
         for (var i=0; i<3; i++) {
           if ($('#img' + i).attr('src') == '') {
             $('#img' + i).css('display', 'none');
@@ -213,6 +235,7 @@ function ViewModel () {
           }
         }
       },
+      //If the request fails, show error message
       error: function (jqXHR, textStatus, errorThrown) {
         if (textStatus == 'error') {
           $('.img-error').text('Error: The requested images are unavailable.');
@@ -223,6 +246,7 @@ function ViewModel () {
 
     $.ajax({ url: 'https://api.foursquare.com/v2/venues/' + place.vid + '/tips?&client_id=IISH2ZQK5FLY3F4GM0P4SUQN4EXQV5ZENQMBSD1POZ0AABOO&client_secret=OKCZBXIZOLZI4E1M55VIOSD2CL3UH1YJAMDPEXWR1FFLXNDZ&v=20150305', 
       success: function (response) {
+        //create an element for each tip and append it the the tip-list div
         $('.tip-list').empty();
         var len = response.response.tips.items.length;
         for (var i=0; i<len; i++) {
@@ -230,6 +254,7 @@ function ViewModel () {
           $('.tip-list').append("<p>" + text + "</p>");
         }
       },
+      //If the request fails, show error message
       error: function (jqXHR, textStatus, errorThrown) {
         if (textStatus == 'error') {
           $('.tip-error').text('Error: The requested data is unavailable.');
@@ -238,9 +263,8 @@ function ViewModel () {
     })
   }
 
+  //call initialize() when the page loads
   google.maps.event.addDomListener(window, 'load', initialize);
 }
 
 ko.applyBindings(new ViewModel());
-
-//&client_id=IISH2ZQK5FLY3F4GM0P4SUQN4EXQV5ZENQMBSD1POZ0AABOO&client_secret=OKCZBXIZOLZI4E1M55VIOSD2CL3UH1YJAMDPEXWR1FFLXNDZ&v=20150305'
